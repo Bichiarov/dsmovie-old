9e969d1bf1a6d329d14a50244c32a8b49b1e5b07
Alex <alexander.bichiarov@gmail.com>
2022-01-14 18:27:49 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
@@ -18,23 +18,25 @@ public class ScoreService {
 	
 	@Autowired
 	private MovieRepository movieRepository;
-
+	
+	
 	@Autowired
 	private UserRepository userRepository;
+	
 	
 	@Autowired
 	private ScoreRepository scoreRepository;
 	
+	
 	@Transactional
 	public MovieDTO saveScore(ScoreDTO dto) {
 		
-		User user = userRepository.findBYEmail(dto.getEmail());
+		User user = userRepository.findByEmail(dto.getEmail());
 		if (user == null) {
 			user = new User();
 			user.setEmail(dto.getEmail());
-			user = userRepository.saveAndFlush(user);			
+			user = userRepository.saveAndFlush(user);
 		}
-		
 		Movie movie = movieRepository.findById(dto.getMovieId()).get();
 		
 		Score score = new Score();
@@ -48,21 +50,13 @@ public class ScoreService {
 		for (Score s : movie.getScores()) {
 			sum = sum + s.getValue();
 		}
-		
-		double avg = sum/ movie.getScores().size();
-		
+		double avg = sum / movie.getScores().size();
 		movie.setScore(avg);
 		movie.setCount(movie.getScores().size());
-		
 		movie = movieRepository.save(movie);
-		
 		return new MovieDTO(movie);
-		
-		
 	}
-
+	
 	
 
-		
-	}
-
+}
